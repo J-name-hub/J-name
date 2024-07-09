4a8da6381b9a9adb48bb72eecab72553877d6582
J-name-hub <gusckss@naver.com>
2024-07-10 00:06:09 +0900

    Update cal_par_st.py

--- cal_par_st.py
+++ cal_par_st.py
@@ -167,7 +167,7 @@ for day in month_days:
         if date_str not in schedule_data:
             schedule_data[date_str] = get_shift(date, st.session_state.get("team", "A"))
         background = shift_colors[schedule_data[date_str]]
-        day_style = "font-weight: bold; text-align: center; padding: 1px; height: 50px; font-size: 18px;"  # Adjust padding to minimize spacing
+        day_style = "font-weight: bold; text-align: center; padding: 1px; height: 55px; font-size: 18px;"  # Adjust padding to minimize spacing
 
         if date.date() == today.date():  # Check if the date is today
             background = "background-color: lightblue"
@@ -181,7 +181,7 @@ for day in month_days:
         shift_text = f"<div style='color: black'>{day[2]}<br><span style='color: black;'>{schedule_data[date_str] if schedule_data[date_str] != '비' else '&nbsp;'}</span></div>"  # Always black text for shift
         week.append(f"<div style='{background}; {day_style}'>{shift_text}</div>")
     else:
-        week.append("<div style='height: 50px;'>&nbsp;</div>")  # Ensure empty cells also have the same height
+        week.append("<div style='height: 55px;'>&nbsp;</div>")  # Ensure empty cells also have the same height
     
     if day[3] == 6:  # End of the week
         calendar_df.loc[len(calendar_df)] = week
