19b6db596ca79c779bdc3bc11eda8937e8d51b57
J-name-hub <gusckss@naver.com>
2024-07-10 11:43:21 +0900

    Update cal_par_st.py

--- cal_par_st.py
+++ cal_par_st.py
@@ -124,7 +124,7 @@ def get_shift(target_date, team):
 # 1페이지: 달력 보기
 
 # CSS 스타일 정의
-title_style = "font-size: 24px; font-weight: bold; text-align: center; color: black;"
+title_style = "font-size: 24px; font-weight: bold; text-align: center;"
 
 # st.markdown을 사용하여 CSS 스타일 적용
 st.markdown(f"<div style='{title_style}'>{month}월 교대근무 달력</div>", unsafe_allow_html=True)
