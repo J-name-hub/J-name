c8a68f37443dc05fa4995a53ee07c8187e14628f
J-name-hub <gusckss@naver.com>
2024-07-16 08:21:30 +0900

    Update cal_par_st.py

--- cal_par_st.py
+++ cal_par_st.py
@@ -203,7 +203,6 @@ calendar_df = pd.DataFrame(calendar_data, columns=["월", "화", "수", "목", "
 
 # 요일 헤더 스타일 설정
 days_header = ["월", "화", "수", "목", "금", "토", "일"]
-days_header_style = "font-weight: bold; text-align: center; background-color: lightgray; height: 50px; font-size: 18px;"
 
 # 달력 데이터 생성
 calendar_data = []
