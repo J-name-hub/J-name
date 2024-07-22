7d3ac23c96882b9e253b8ed51f7be6db4b93f094
J-name-hub <gusckss@naver.com>
2024-07-22 13:18:20 +0900

    Update lightning.py

--- lightning.py
+++ lightning.py
@@ -67,7 +67,7 @@ def get_lightning_data(datetime_str):
             'serviceKey': API_KEY,
             'numOfRows': '1000',
             'pageNo': '1',
-            'lgtType': '1',
+            'lgtType': '2',
             'dateTime': datetime_str
         }
         response = requests.get(API_URL, params=params)
