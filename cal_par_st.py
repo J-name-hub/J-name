7ed0a1fea0dbc3993b1e526406c063530d40f203
J-name-hub <gusckss@naver.com>
2024-07-30 14:28:48 +0900

    Update cal_par_st.py

--- cal_par_st.py
+++ cal_par_st.py
@@ -267,7 +267,7 @@ def main():
             text-align: center;
             padding: 2px;
             height: 60px;  /* 높이를 약간 늘렸습니다 */
-            font-size: 17px;
+            font-size: 18px;
             display: flex;
             flex-direction: column;
             justify-content: center;
