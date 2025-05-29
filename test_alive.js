623c78dd1014ab4e7d47fc7c3b76b79e99d95afc
J-name-hub <gusckss@naver.com>
2025-05-29 10:42:55 +0900

    Update test_alive.js

--- test_alive.js
+++ test_alive.js
@@ -1,1 +1,14 @@
+const puppeteer = require('puppeteer');
 
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+  const page = await browser.newPage();   // 새 탭 열기
+  await page.goto('https://testforcal.streamlit.app', { waitUntil: 'networkidle2' });   // 페이지 열기
+
+  await browser.close();   // 브라우저 종료
+
+})();
