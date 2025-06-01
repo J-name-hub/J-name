b33e90ffb5f2ccad86f2db1b30214bd2c58111f6
J-name-hub <gusckss@naver.com>
2025-06-01 17:08:33 +0900

    streamlit_keep_alive.js 업데이트

--- streamlit_keep_alive.js
+++ streamlit_keep_alive.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 
 // ========== 설정 상수 ==========
 const STREAMLIT_URLS = [
-  'https://calendarforpar.streamlit.app',
-  'https://train-config.streamlit.app'
+  'https://train-config.streamlit.app',
+  'https://yeongjongdonakreomap.streamlit.app'
+  // 'https://calendarforpar.streamlit.app'
   // 추가 URL이 있으면 여기에 추가
   // 'https://another-app.streamlit.app'
 ];
