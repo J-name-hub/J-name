ff6f1029b0531ebeff1be8ad1129c4c087e2d9e6
J-name-hub <gusckss@naver.com>
2025-07-02 02:28:22 +0900

    Update 📅 교대근무 달력.py

--- 📅 교대근무 달력.py
+++ 📅 교대근무 달력.py
@@ -644,7 +644,7 @@ def sidebar_controls(year, month, schedule_data):
             team = st.selectbox("조 선택", available_teams, index=available_teams.index(default_team))
             change_start_date = st.date_input("적용 시작일", datetime.today(), key="start_date")
             password_for_settings = st.text_input("암호 입력", type="password", key="settings_password")
-            col1, col2, col3 = st.columns([1, 1, 1])
+            col1, col2, col3 = st.columns([1, 4, 1])
             with col3:
                 submit_button = st.form_submit_button("조 설정 저장")
 
@@ -682,7 +682,7 @@ def sidebar_controls(year, month, schedule_data):
                     change_date = st.date_input("변경할 날짜", datetime(st.session_state.year, st.session_state.month, 1), key="change_date")
                     new_shift = st.selectbox("새 스케줄", ["주", "야", "비", "올"], key="new_shift")
                     password = st.text_input("암호 입력", type="password", key="password")
-                    col1, col2, col3 = st.columns([1, 1, 1])
+                    col1, col2, col3 = st.columns([1, 4, 1])
                     with col3:
                         change_submit_button = st.form_submit_button("스케줄 변경 저장")
     
