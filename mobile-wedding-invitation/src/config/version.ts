bd1c148bbac719852174bd884c1424ec0c5fbe21
J-name-hub <gusckss@naver.com>
2025-12-27 22:30:45 +0900

    Delete mobile-wedding-invitation directory

--- mobile-wedding-invitation/src/config/version.ts
+++
@@ -1,1 +0,0 @@
-export const buildVersion = "dev" as const;
--- mobile-wedding-invitation/src/styles/theme.ts
+++
@@ -1,6 +0,0 @@
-export const theme = {
-  maxWidth: "var(--maxw)",
-  radiusLg: "var(--radius-lg)",
-  radiusMd: "var(--radius-md)",
-  shadow: "var(--shadow)"
-} as const;
