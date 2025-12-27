2c59a823e0beb6cd9ad75fa52928970972182160
J-name-hub <gusckss@naver.com>
2025-12-27 21:42:43 +0900

    Create registry.tsx

---
+++ mobile-wedding-invitation/src/lib/registry.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+import { GlobalStyle } from "@/src/styles/globalStyles";
+
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+  const [sheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = sheet.getStyleElement();
+    sheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") {
+    return (
+      <>
+        <GlobalStyle />
+        {children}
+      </>
+    );
+  }
+
+  return (
+    <StyleSheetManager sheet={sheet.instance}>
+      <>
+        <GlobalStyle />
+        {children}
+      </>
+    </StyleSheetManager>
+  );
+}
